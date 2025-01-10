66f5f7fa787811890b9b281d4c8ffd63cde44de1
Piotr Brudny <pbrudny@gmail.com>
2025-01-10 10:34:32 +0000

    Move events to folder

---
+++ events/appointment_confirmation_event.py
@@ -0,0 +1,5 @@
+from events.event import Event
+
+class AppointmentConfirmationEvent(Event):
+    def __init__(self, passport_number, is_confirmed):
+        super().__init__("appointment_confirmation", {"passport_number": passport_number, "is_confirmed": is_confirmed})
